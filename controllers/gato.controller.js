e5fd1c73eb59753685ff21f23021a84b31da2e98
jjrm-29 <juanjosereyesm16@gmail.com>
2026-08-17 12:13:38 -0600

    corregido

--- controllers/gato.controller.js
+++ controllers/gato.controller.js
@@ -79,27 +79,7 @@ export const registrarGato = async (req, res) => {
   }
 };
 
-Reemplace el código de routes/gato.routes.js con lo siguiente:
 
-import express from "express";
-import multer from "multer";
-import { registrarGato } from "../controllers/gato.controller.js";
 
-const router = express.Router();
-
-const upload = multer({
-  storage: multer.memoryStorage(),
-  limits: {
-    fileSize: 5 * 1024 * 1024,
-  },
-});
-
-router.post(
-  "/gato",
-  upload.single("imagen"),
-  registrarGato
-);
-
-export default router;
 
 
